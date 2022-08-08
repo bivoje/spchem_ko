29b898aea9c0869ffd2c83b973f0455b9db2812b
bivoje <bivoje49@gmail.com>
2022-08-08 11:45:25 +0900

    Add fontpages_script_gen

---
+++ fontpages_cmd_gen.sh
@@ -0,0 +1,4 @@
+
+for size in ; do
+  echo 'C:\Program Files\ImageMagick-7.1.0-Q16-HDRI\convert.exe -background black -fill white -font Desktop\fontpages\NEOHPEN3.TTF -pointsize '$size' label:"q  w  e  r  t  y  u  i  o  p  a  s  d  f  g  h  j  k  l  z  x  c  v  b  n  m  Q  W  E  R  T  O  P" Desktop\'$size'.png'
+done
